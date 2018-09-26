939f36e75584b139f06dc033713a9105778461f5
heikopiirme1 <heiko.piirm@khk.ee>
2018-09-26 20:09:13 +0300

    Allowed token headers

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.json());
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-access-token');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
 });
